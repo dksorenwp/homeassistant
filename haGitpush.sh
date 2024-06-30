9a5669a4f6200a05d4a2701fe76765d332475bb7
root <root@homeassistant.local.hass.io>
2024-06-30 17:55:48 +0200

    Auto Backup - 30-06-2024 17:55:48

--- haGitpush.sh
+++ haGitpush.sh
@@ -1,6 +1,6 @@
 # Go to /config folder or 
 # Change this to your Home Assistant config folder if it is different
-cd /config
+cd /root/config
 # Add all files to the repository with respect to .gitignore rules
 git add .
 # Commit changes with message with current date stamp
