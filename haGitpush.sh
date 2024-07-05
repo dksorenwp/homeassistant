4597c629a7dab0a385ad282a4eff0d7ff9bdacd6
root <root@homeassistant.local.hass.io>
2024-07-05 23:31:30 +0200

    Auto Backup - 05-07-2024 23:31:30

--- haGitpush.sh
+++ haGitpush.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+# Debug: Log file path
+LOG_FILE="/config/gitPush.log"
+
+# Debug: Print date and time of script execution
+echo "Script execution started at $(date)" >> "$LOG_FILE"
+
 # Set core.sshCommand for Git to use specific SSH key and ignore SSH configuration
 git config core.sshCommand "ssh -i /config/.ssh/id_rsa -F /dev/null"
 
+# Debug: Print current directory
+echo "Current directory: $(pwd)" >> "$LOG_FILE"
+
 # Add all files to the repository respecting .gitignore rules
-git add .
+/usr/bin/git add . >> "$LOG_FILE" 2>&1
+
+# Debug: Print git status before commit
+/usr/bin/git status >> "$LOG_FILE" 2>&1
 
 # Commit changes with a message including the current date and time stamp
-git commit -m "Auto Backup - $(date +'%d-%m-%Y %H:%M:%S')"
+/usr/bin/git commit -m "Auto Backup - $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOG_FILE" 2>&1
+
+# Debug: Print git log after commit
+/usr/bin/git log -1 >> "$LOG_FILE" 2>&1
 
 # Push changes to GitHub (assuming 'origin' points to your GitHub repository and 'master' is your branch)
-git push -u origin master
+/usr/bin/git push -u origin master >> "$LOG_FILE" 2>&1
+
+# Debug: Print git push output
+echo "Git push exit code: $?" >> "$LOG_FILE"
+
+# Debug: Print date and time of script completion
+echo "Script execution completed at $(date)" >> "$LOG_FILE"
