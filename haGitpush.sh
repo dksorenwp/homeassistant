592d96393b15a708c8daf4e6d8a25a3db3db3616
root <root@a0d7b954-ssh.local.hass.io>
2024-07-05 22:44:33 +0200

    Auto Backup - 05-07-2024 22:44:33

--- haGitpush.sh
+++ haGitpush.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# Go to /config folder or 
-# Change this to your Home Assistant config folder if it is different
+
+# Go to /config folder or change this to your Home Assistant config folder if it is different
 cd /root/config
-# Add all files to the repository with respect to .gitignore rules
+
+# Add all files to the repository respecting .gitignore rules
 git add .
-# Commit changes with message with current date stamp
-git commit -m "Auto Backup - `date +'%d-%m-%Y %H:%M:%S'`"
-# Push changes towards GitHub
+
+# Commit changes with a message including the current date and time stamp
+git commit -m "Auto Backup - $(date +'%d-%m-%Y %H:%M:%S')"
+
+# Push changes to GitHub (assuming 'origin' points to your GitHub repository and 'master' is your branch)
 git push -u origin master
